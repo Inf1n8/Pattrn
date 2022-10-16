0375b8a141f7425f79137718d99cf7c3c7f4c466
Vikrame Vasudev <vikrame1999@gmail.com>
2022-10-15 23:59:20 -0700

    Designed login page and integrated with backend endpoint to authenticate user

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,1 +1,3 @@
-export const API_URL = "https://backend-dot-cal-hacks-90.wn.r.appspot.com";
+// export const API_URL = "https://backend-dot-cal-hacks-90.wn.r.appspot.com";
+export const API_URL = "http://localhost:8080";
+export const LOGIN_URL = "/login";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import ProtectedRoute from "./ProtectedRoute";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<ProtectedRoute />}>
+        <Route exact path="/" element={<App />} />
+      </Route>
+      <Route exact path="/login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
